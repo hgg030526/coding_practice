43788488288e51ecb1a24edf72887cdf92e481c1
hgg030526 <wqr754x68q@privaterelay.appleid.com>
2026-02-06 23:37:26 +0900

    Update 최빈값 구하기.py

--- 프로그래머스/Lv.0/최빈값 구하기.py
+++ 프로그래머스/Lv.0/최빈값 구하기.py
@@ -9,3 +9,14 @@ def solution(array):
         answer = list.index(max(list))
     
     return answer
+
+
+
+
+#다른 풀이 : [1 2 2 3 3 3] -> [2 3 3] -> [3]
+def solution(array):
+    while len(array) != 0:
+        for i, a in enumerate(set(array)):
+            array.remove(a)
+        if i == 0: return a
+    return -1
