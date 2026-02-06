c61fd8517e656de3903db63191d5b654b0de435b
hgg030526 <wqr754x68q@privaterelay.appleid.com>
2026-02-06 23:35:16 +0900

    Update 최빈값 구하기.py

--- 프로그래머스/Lv.0/최빈값 구하기.py
+++ 프로그래머스/Lv.0/최빈값 구하기.py
@@ -1,5 +1,5 @@
 def solution(array):
-    list = [0 for x in range(1000)]
+    list = [0 for x in range(1000)]  #[0]*1000   #list 대신 cnt, lst 사용하기
     for num in array:
         list[num] = list[num] + 1
     
