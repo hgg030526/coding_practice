fc0127629a894d25bd82510cefbc90147b5f8f4b
hgg030526 <wqr754x68q@privaterelay.appleid.com>
2026-02-13 14:28:53 +0900

    Create 문자열 정렬하기 (2).py

---
+++ 프로그래머스/Lv.0/문자열 정렬하기 (2).py
@@ -0,0 +1,13 @@
+def solution(my_string):
+    answer = ''.join(sorted([s.lower() for s in my_string]))
+    return answer
+
+
+def solution(my_string):
+    answer = ''.join(sorted(list(my_string.lower())))
+    return answer
+
+
+def solution(my_string):
+    answer = ''.join(sorted(my_string.lower()))
+    return answer
