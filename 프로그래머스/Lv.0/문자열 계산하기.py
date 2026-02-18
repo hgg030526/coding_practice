9c52250e2ea60283211d1bbe19db8305bbc75e3b
hgg030526 <wqr754x68q@privaterelay.appleid.com>
2026-02-18 23:41:29 +0900

    Update 문자열 계산하기.py

--- 프로그래머스/Lv.0/문자열 계산하기.py
+++ 프로그래머스/Lv.0/문자열 계산하기.py
@@ -17,3 +17,8 @@ def solution(my_string):
             answer -= numbers[i+1]
 
     return answer
+
+
+#다른 풀이 : 9 - 2 를 9 + (-2) 로 푸는 방법
+def solution(my_string):
+    return sum(int(i) for i in my_string.replace(' - ',' + -').split(' + '))
