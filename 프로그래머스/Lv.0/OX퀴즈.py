6bc5badf6d706c3964f40912d9f917bfb3dfe18e
hgg030526 <wqr754x68q@privaterelay.appleid.com>
2026-02-20 23:16:21 +0900

    Update OX퀴즈.py

--- 프로그래머스/Lv.0/OX퀴즈.py
+++ 프로그래머스/Lv.0/OX퀴즈.py
@@ -1,7 +1,7 @@
 def solution(quiz):
     answer = []
     for s in quiz:
-        x,op,y,eq,z = s.split()
+        x,op,y,eq,z = s.split()  # eq는 안쓰는 변수이므로 '_'로 써도 됨
         
         if op == '+':
             answer.append('O' if int(x)+int(y)==int(z) else 'X')
