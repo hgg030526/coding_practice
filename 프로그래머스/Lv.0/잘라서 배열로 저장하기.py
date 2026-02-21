0f45dfea9d00d6a7290deeaa86778d5bb8604951
hgg030526 <wqr754x68q@privaterelay.appleid.com>
2026-02-22 00:16:32 +0900

    Update 잘라서 배열로 저장하기.py

--- 프로그래머스/Lv.0/잘라서 배열로 저장하기.py
+++ 프로그래머스/Lv.0/잘라서 배열로 저장하기.py
@@ -9,3 +9,20 @@ def solution(my_str, n):
         
     answer.append(my_str[n*(i+1):])
     return answer
+
+#두번째 풀이
+def solution(my_str, n):
+    answer,i,end_i = [],0,(len(my_str)-1)//n
+    
+    while i < end_i:
+        answer.append(my_str[n*i:n*(i+1)])
+        i += 1
+        
+    answer.append(my_str[n*end_i:])
+        
+    return answer
+
+
+#다른 풀이 : 슬라이싱은 초과해도 오류가 발생하지 않는다
+def solution(my_str, n):
+    return [my_str[i:i+n] for i in range(0,len(my_str),n)]
